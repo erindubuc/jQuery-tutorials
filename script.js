ff229c292fb18d919a4ada23acdcac78ce004f3c
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-14 17:39:48 +0000

    lesson7 traversing the DOM

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-// first and last
-// $("header nav li:first").css({border: "2px solid red"});
-// $("header nav li:last").css({border: "2px solid red"});
+// next
+// $("#contact-methods").next().css({border: "3px solid red"});
 
-// first-child and last-child
-// $("#contact ul:first").css({border: "2px solid red"});
-// $("#contact ul:last").css({border: "2px solid red"});
+// prev
+// $("#social-nav").prev().css({border: "3px solid blue"});
 
-// even
-// $("header nav li:even").css({border: "2px solid blue"});
-// odd
-// $("header nav li:odd").css({border: "2px solid yellow"});
+// // parent
+// $(".banner-title").parents().css({border: "3px solid pink"});
 
-// not -get sections that do NOT match #contact
-// $("section:not('#contact')").css({border: "2px solid green"});
+// // children 
+// $("#social-nav").children().css({border: "3px solid green"});
 
-// less than
-// $("#social-nav li:lt(3)").css({border: "2px solid blue"});
-// // greater than
-// $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+// // find
+// $("#contact").find(".facebook").css({border: "3px solid purple"});
 
-// attribute filters
-// $("div[class]").css({border: "2px solid pink"});
-
-// attribute with specific value
-$("img[alt=quote]").css({border: "2px solid purple"});
-
-
+// closest
+$("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
