992a5cd3609941e6fcb1ad1f14eefc3159acd926
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-14 17:14:50 +0000

    lesson6 using filters to change css

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-$("h3").css({border: "3px solid blue"});
+// first and last
+// $("header nav li:first").css({border: "2px solid red"});
+// $("header nav li:last").css({border: "2px solid red"});
 
-$(".wrapper").css({border: "3px solid red"});
+// first-child and last-child
+// $("#contact ul:first").css({border: "2px solid red"});
+// $("#contact ul:last").css({border: "2px solid red"});
 
-$("#clients").css({border: "3px solid yellow"});
+// even
+// $("header nav li:even").css({border: "2px solid blue"});
+// odd
+// $("header nav li:odd").css({border: "2px solid yellow"});
+
+// not -get sections that do NOT match #contact
+// $("section:not('#contact')").css({border: "2px solid green"});
+
+// less than
+// $("#social-nav li:lt(3)").css({border: "2px solid blue"});
+// // greater than
+// $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+
+// attribute filters
+// $("div[class]").css({border: "2px solid pink"});
+
+// attribute with specific value
+$("img[alt=quote]").css({border: "2px solid purple"});
+
+
