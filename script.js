637726cdac23e4909ca4f4b36dbe04562c2f821c
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-14 17:46:04 +0000

    lesson8 chaining

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-// next
-// $("#contact-methods").next().css({border: "3px solid red"});
+// set-up for chaining
+// don't overdo it with too many changes in the chain
 
-// prev
-// $("#social-nav").prev().css({border: "3px solid blue"});
-
-// // parent
-// $(".banner-title").parents().css({border: "3px solid pink"});
-
-// // children 
-// $("#social-nav").children().css({border: "3px solid green"});
-
-// // find
-// $("#contact").find(".facebook").css({border: "3px solid purple"});
-
-// closest
-$("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
+$("#contact-methods").css({border: "2px solid red"})
+    .next().css({border: "2px solid green"})
+    .closest("section").css({border: "2px solid blue"});
