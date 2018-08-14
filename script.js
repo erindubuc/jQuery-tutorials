da06ea5f27e96cd2db0f9eb5ea57a7cbae31515d
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-14 16:46:31 +0000

    lesson5 added selectors

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-$(document).ready(function() {
-    alert("jquery loaded");
-});
+$("h3").css({border: "3px solid blue"});
+
+$(".wrapper").css({border: "3px solid red"});
+
+$("#clients").css({border: "3px solid yellow"});
