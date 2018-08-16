b4427a6cc0f3ab1b0c3acc378fc79c5edfefcc28
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-16 16:59:20 +0000

    lesson11 removing content with empty and remove

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-var wrapper = "<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
-
-button[0].onclick = function() {
-    
-    if(wrapped){
-        $("section").unwrap();
-        wrapped = false;
-        button.text("Wrap");
-    } else {
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("Unwrap");
-    }
-    
-};
+$(".button").remove();
+$("#contact img").remove();
 
 
-
-
-
-
-
-// $("section").wrap("<div>");
-// $("section").unwrap();
-// $("section").wrapAll("<div>");
-
-/*
-wrap() wraps all matched elements individually
-unwrap() unwraps all matched elements
-wrapALL() wraps all elements combined with 1 single element
-
-*/
+/*.empty() empties the inner HTML of an element
+.remove() removes the element completely*/
