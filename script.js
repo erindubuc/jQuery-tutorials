ecc84f27e9079ed7d3d71cfed4c8cbb8ce9de59d
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-16 17:50:43 +0000

    lesson14 working with classes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-$("#social-nav").css({
-    "top" : "-400px",
-    "left" : "150px",
-    "opacity" : "0.5", 
-    "border-top" : "4px solid red"
-});
+var button = $("#lead-banner a");
+
+button[0].onclick = function() {
+    
+  $("#points-of-sale").toggleClass("open");
+  return false;
+    
+};
+
+
+/*
+removeClass() removes a class from the matched element(s)
+addClass() adds a class to the matched element(s)
+toggleClass() toggles the class on and off on the matched element(s)
+*/
