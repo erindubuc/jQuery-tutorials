dda06152a6f53bf856b200cf39cfa5c96c429e0f
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-22 02:34:44 +0000

    lesson18 access event object during events

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-
-// when the dom is ready to bind events to, then fire or bind the events
-$(document).on("ready", function() {
+$(document).ready(function() {
    
-    
-});
+   $("*").on("click", function(e){
+    //   do something
+        console.log(e.target);
+        console.log("The event type is: " + e.type);
+        // x and y are useful if you're working on a game where you'd want to know where the player clicked for an event to happen
+        console.log("X co-ordinate of the event is: " + e.pageX);
+         console.log("Y co-ordinate of the event is: " + e.pageY);
+       e.stopPropagation();
+   });
 
-// when document is ready, do whatever you need to do...
-$(function(){
-    
-    // what you want to happen when document is ready
-   
-    
-});
 
-// waits for the whole web page to load - if you have an image, it needs to load and THEN the event will fire
-    //e.g. may want to calculate the height of an image if it's sliding in
-$(window).on("load", function() {
-    
-    
-});
-
-$(window).load(function() {
-    
 });
