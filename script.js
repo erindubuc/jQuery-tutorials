184f3009e9db98247643b9c992350a140dd5bbef
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-22 02:20:13 +0000

    lesson17 document ready vs window load

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-// binding
-// $("#lead-banner").click(function() {
-    // alert("you double clicked me");
-// });
 
-// unbinding 
-$("#lead-banner").dblclick(function() {
-    alert("you double clicked me");
-    $("#lead-banner").off("dblclick");
+// when the dom is ready to bind events to, then fire or bind the events
+$(document).on("ready", function() {
+   
+    
 });
 
-/*
-on() binds an event to matched element(s)
-off() unbinds an event from matched element(s)
-*/
+// when document is ready, do whatever you need to do...
+$(function(){
+    
+    // what you want to happen when document is ready
+   
+    
+});
+
+// waits for the whole web page to load - if you have an image, it needs to load and THEN the event will fire
+    //e.g. may want to calculate the height of an image if it's sliding in
+$(window).on("load", function() {
+    
+    
+});
+
+$(window).load(function() {
+    
+});
