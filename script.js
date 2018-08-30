d2f028071989302a890a78604560be330f6b203d
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-30 17:10:16 +0000

    lesson22 sliding elements with up, down, & toggle

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-   function complete() {
-       alert("Animation complete");
-   }
-   $("img[alt=map]").on("click", function(){
-   
-        $("section > h2").toggle(1000);
-            // toggle fades element in or out, depending on its current state
-            // shows/hides too
-            // NOT the same as toggle class which changes the class of an element!!!!
-   
+  
+  $(".slide-button-up").on("click", function() {
+      $("#lead-banner").slideToggle(1000);
+  });
+  
    });
-});
+// slideUp() moves the element up the page
+// slideDown() moves the element down or back down
+// slideToggle() lets you use the same element to slide up and then down when clicked
