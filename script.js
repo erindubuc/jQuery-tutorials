b62475b882ea416f85f22fcfbc65121fc422fc57
Erin Dubuc <erin.dubuc9@gmail.com>
2018-08-30 14:58:50 +0000

    lesson19 animate vs css

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
+   function complete() {
+       alert("Animation complete");
+   }
+   $("section > h2").on("click", function(){
    
-   $("*").on("click", function(e){
-    //   do something
-        console.log(e.target);
-        console.log("The event type is: " + e.type);
-        // x and y are useful if you're working on a game where you'd want to know where the player clicked for an event to happen
-        console.log("X co-ordinate of the event is: " + e.pageX);
-         console.log("Y co-ordinate of the event is: " + e.pageY);
-       e.stopPropagation();
+   $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", 
+   complete);
+   
    });
-
-
 });
