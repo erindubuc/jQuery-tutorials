4d0e4e1dcfa4b959feebb64a83cc1d6734900ab3
Erin Dubuc <erin.dubuc9@gmail.com>
2018-09-04 02:58:33 +0000

    lesson25 Responsive Slides Plugin

--- script.js
+++ script.js
@@ -2,10 +2,5 @@
 /*global $*/
 $(document).ready(function() {
   
-    var items = $("#points-of-sale li");
-    
-    items.on("click", function() {
-        // want to bind click functions to li items
-        $(this).find("p").slideToggle(500);
-    });
+   $(".rslides").responsiveSlides();
 });
